2fa1d26d94cacef35e9f5f287fa4b91a2048a931
Botond Pelyi <botond.pelyi@gmail.com>
2023-04-19 20:56:23 +0200

    feat: phpactor keymaps

--- vim/dotfiles/config/init.lua
+++ vim/dotfiles/config/init.lua
@@ -155,7 +155,32 @@ local plugins = {
   },
   {
     -- TODO: add build script to run composer install in the installed plugin directory [botond.pelyi]
-    'phpactor/phpactor'
+    'phpactor/phpactor',
+    config = function ()
+      function PhpactorCreateNewObj()
+        vim.api.nvim_call_function(
+          'phpactor#rpc',
+          { 'class_new', { current_path = vim.api.nvim_call_function('phpactor#_path', {}), variant = "default" } }
+        )
+      end
+
+      function PhpactorImplementContract()
+        vim.api.nvim_call_function(
+          'phpactor#rpc',
+          {
+            'transform',
+            {
+              transform = 'implement_contracts',
+              source = vim.api.nvim_call_function('phpactor#_source', {}),
+              path = vim.api.nvim_call_function('phpactor#_path', {})
+            }
+          }
+        )
+      end
+
+      vim.keymap.set('n', '<leader>pn', '<cmd>lua PhpactorCreateNewObj()<cr>')
+      vim.keymap.set('n', '<leader>pi', '<cmd>lua PhpactorImplementContract()<cr>')
+    end
   }
 }
 require("lazy").setup(plugins, {})

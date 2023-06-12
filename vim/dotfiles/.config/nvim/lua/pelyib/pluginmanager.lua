e6109ef75dacf86af6936107ca9412dcf0f7e806
Botond Pelyi <botond.pelyi@byborgenterprises.com>
2023-06-12 08:10:45 +0200

    rafact: change keymap for general terminal

--- vim/dotfiles/.config/nvim/lua/pelyib/pluginmanager.lua
+++ vim/dotfiles/.config/nvim/lua/pelyib/pluginmanager.lua
@@ -138,7 +138,9 @@ M.plugins = {
         config = function ()
             require('toggleterm').setup({
                 direction = 'float',
-                open_mapping = [[<F7>]]
+                --open_mapping = [[<F7>]],
+                open_mapping = [[<leader>t]],
+                auto_scroll = false
             })
         end
     },

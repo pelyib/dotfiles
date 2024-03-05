51421bf31bebf331cf97a9e6a58d5084146b3b5b
Botond Pelyi <botond.pelyi@byborgenterprises.com>
2024-03-05 11:24:34 +0100

    chore: one more thing how to install PHPActor

--- vim/dotfiles/.config/nvim/lua/plugins/phpactor.lua
+++ vim/dotfiles/.config/nvim/lua/plugins/phpactor.lua
@@ -6,6 +6,7 @@ return {
         TODO: add build script to run composer install in the installed plugin directory [botond.pelyi]
         cd into vim.fn.stdpath("data") .. '/lazy/phpactor'
         then run the composer install --no-dev --ignore-plaform-reqs
+        -d, --working-dir=WORKING-DIR                        If specified, use the given directory as working directory.
         --]]
     end,
 }

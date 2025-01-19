add453d255e11deaca64865274c7fd350b68444e
pelyib <botond.pelyi@gmail.com>
2025-01-19 21:33:15 +0100

    feat(nvim): add CSV support, reformat the line

--- vim/dotfiles/.config/nvim/lua/pelyib/statusline.lua
+++ vim/dotfiles/.config/nvim/lua/pelyib/statusline.lua
@@ -1,14 +1,51 @@
 local M = {}
 
 M.calc =function ()
-    -- TODO if the buff is not a file or the file is not saved return a simplified statusline
-
     local root = vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+
+    if vim.bo.buftype == "nofile" or vim.bo.buftype == "quickfix" then
+        return string.format(
+            "%s | [  %s]",
+            M.branch(),
+            root
+        )
+    end
+
     local relative_path = vim.fn.expand("%:.")
     local current_line_length = #vim.fn.getline('.')
+    -- [project/root folder]{relative path}::{namespace.class}.{method}            {line num of curs} / {all lines num} | {col of curs}-{len of row} | percent of cursor position
+    return string.format(
+        "%s | [  %s]%s%s %%=%%l / %%L | %%v-%d | %%p %%",
+        M.branch(),
+        root,
+        relative_path,
+        M.code(),
+        current_line_length
+    )
+end
+
+M.branch = function ()
+    if vim.b.statusline_branch ~= nil then
+        return vim.b.statusline_branch
+    end
+
+    local branch = vim.fn.system("git branch --show-current 2> /dev/null | tr -d '\n'")
+    if branch ~= "" then
+        vim.b.statusline_branch = string.format(
+            "  %s",
+            branch
+        )
+        return vim.b.statusline_branch
+    else
+        return ""
+    end
+end
+
+M.code = function ()
     local code_first_lev = ""
     local code_second_lev = ""
     local code_path = ""
+    -- TODO: it does not work without the barbecue plugin
     local code = require("nvim-navic").get_data()
     if code ~= nil then
 
@@ -48,14 +85,7 @@ M.calc =function ()
         end
     end
 
-    -- [project/root folder]{relative path}::{namespace.class}.{method}            {line num of curs} / {all lines num} | {col of curs}-{len of row} | percent of cursor position
-    return string.format(
-        "[%s]%s%s %%=%%l / %%L | %%v-%d | %%p %%",
-        root,
-        relative_path,
-        code_path,
-        current_line_length
-    )
+    return code_path
 end
 
 return M

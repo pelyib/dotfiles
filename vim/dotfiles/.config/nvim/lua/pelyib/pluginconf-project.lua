3fe3c93e49a23df89e1d88ebadcbafd86ac5a928
Botond Pelyi <botond.pelyi@gmail.com>
2026-02-10 17:08:53 +0100

    fix: project config file load

--- vim/dotfiles/.config/nvim/lua/pelyib/pluginconf-project.lua
+++ vim/dotfiles/.config/nvim/lua/pelyib/pluginconf-project.lua
@@ -6,9 +6,9 @@ M.setup = function()
 	local cwd = vim.fn.getcwd()
 	local projectModule = cwd .. "/.local.lua"
 	if vim.fn.filereadable(vim.fn.expand(projectModule)) == 1 then
-		local ok, err = pcall(vim.cmd.luafile, projectModule)
-		if ok and _G.projectConf and _G.projectConf.config then
-			M.config = _G.projectConf.config
+		local ok, projectConf = pcall(dofile, projectModule)
+		if ok and projectConf and projectConf.config then
+			M.config = projectConf.config
 		elseif not ok then
 			vim.notify("Failed to load project config: " .. tostring(err), vim.log.levels.WARN)
 		end

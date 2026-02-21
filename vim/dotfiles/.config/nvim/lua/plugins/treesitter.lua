83d9995dc1bebba35c9db3d3c37f013d55ebe5e1
Botond Pelyi <botond.pelyi@gmail.com>
2026-02-21 16:22:13 +0100

    feat[nvim]: update and migrate to builtin treesitter

--- vim/dotfiles/.config/nvim/lua/plugins/treesitter.lua
+++ vim/dotfiles/.config/nvim/lua/plugins/treesitter.lua
@@ -5,56 +5,77 @@ else
 	pluginconf = pluginconf.config.patched
 end
 
+-- Since treesitter is a builtin feature of Neovim nvim-treesitter purpose has been changed to be a helper for
+-- installing parsers and providing some extra features like folding and indenting. So we can disable it by default and
+-- only enable it when we need it, which is when we open a filetype that has a treesitter parser available.
 return vim.tbl_deep_extend("force", {
 	"nvim-treesitter/nvim-treesitter",
 	enabled = false,
-	lazy = true,
-	event = { "BufReadPost", "BufNewFile" },
+	lazy = false,
 	-- Swtich to a specific tag later when they release a new version that contains the tree-sitter-php v0.22.5
 	-- that fixes this issue: https://github.com/tree-sitter/tree-sitter-php/issues/243
 	--tag = 'v0.9.2',
-	commit = "caf275382f91ec8a2498d455c4d7d3fd0265ebd3",
+	commit = "dc42c209f3820bdfaae0956f15de29689aa6b451",
 	build = ":TSUpdate",
 	config = function()
-		vim.cmd([[syntax off]])
-		local treesitter_ok, treesitter = pcall(require, "nvim-treesitter.configs")
-		if not treesitter_ok then
-			return
-		end
-		treesitter.setup({
-			ensure_installed = {
-				"bash",
-				"cmake",
-				"css",
-				"dockerfile",
-				"dot",
-				"go",
-				"graphql",
-				"html",
-				"hcl",
-				"javascript",
-				"json",
-				"lua",
-				"luadoc",
-				"make",
-				"markdown",
-				"php",
-				"python",
-				"regex",
-				"sql",
-				"typescript",
-				"vim",
-				"vimdoc",
-				"yaml",
-			},
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			},
-			indent = {
-				enable = true,
-			},
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "*",
+			callback = function(event)
+				-- make sure nvim-treesitter is loaded
+				local ok, nvim_treesitter = pcall(require, "nvim-treesitter")
+
+				-- no nvim-treesitter, maybe fresh install
+				if not ok then
+					return
+				end
+
+				local parsers = require("nvim-treesitter.parsers")
+
+				if not parsers[event.match] or not nvim_treesitter.install then
+					return
+				end
+
+				local ft = vim.bo[event.buf].ft
+				local lang = vim.treesitter.language.get_lang(ft)
+				nvim_treesitter.install({ lang }):await(function(err)
+					if err then
+						vim.notify("Treesitter install error for ft: " .. ft .. " err: " .. err)
+						return
+					end
+
+					pcall(vim.treesitter.start, event.buf)
+					-- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					-- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				end)
+			end,
 		})
+
+		require("nvim-treesitter").install({
+			"bash",
+			"cmake",
+			"css",
+			"dockerfile",
+			"dot",
+			"go",
+			"graphql",
+			"html",
+			"hcl",
+			"javascript",
+			"json",
+			"lua",
+			"luadoc",
+			"make",
+			"markdown",
+			"php",
+			"python",
+			"regex",
+			"sql",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		})
+
 		vim.treesitter.language.register("typescript", "typescriptreact")
 		vim.treesitter.language.register("hcl", "terraform")
 	end,
